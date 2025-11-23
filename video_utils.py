66daf42a336610a842c07cffd34e3b5dcd90aeab
jilanirabbani1234-coder <jilanirabbani1234@gmail.com>
2025-11-23 11:29:08 +0530

    Update video_utils.py

--- video_utils.py
+++ video_utils.py
@@ -1,1 +1,11 @@
+import os
+from moviepy.editor import ImageSequenceClip, VideoFileClip
 
+def frames_to_video(frame_paths, out_path, fps=8):
+    clip = ImageSequenceClip(frame_paths, fps=fps)
+    clip.write_videofile(out_path, codec="libx264", fps=fps, verbose=False, logger=None)
+    return out_path
+
+def clip_to_stream(path):
+    # For streaming if needed
+    return open(path, "rb")
